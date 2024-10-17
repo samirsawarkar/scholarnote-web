28dd969cccf4b7f54f52841523fc590a2477d6d5
samir sawarkar <96814673+samirsawarkar@users.noreply.github.com>
2024-10-17 17:39:39 +0530

    Add firebaseConfig.js for Firebase initialization (npm build and npm start working )

---
+++ src/app/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSC91UhsaFsYEBTSFQr9V1JXtgg31Abeo",
+  authDomain: "scholarnote-73796.firebaseapp.com",
+  projectId: "scholarnote-73796",
+  storageBucket: "scholarnote-73796.appspot.com",
+  messagingSenderId: "293851557107",
+  appId: "1:293851557107:web:e97d3b3573c31468d0db86"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
